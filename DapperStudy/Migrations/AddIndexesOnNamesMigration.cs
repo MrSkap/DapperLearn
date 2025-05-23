8f0e3fa25d576929b46b46f7dc89c476c720cfa7
kozlov.stepan <2001skap@gmail.com>
2025-05-23 16:59:44 +0300

    add name indexes migration

---
+++ DapperStudy/Migrations/AddIndexesOnNamesMigration.cs
@@ -0,0 +1,25 @@
+﻿using FluentMigrator;
+
+namespace DapperStudy.Migrations;
+
+[Migration(5)]
+public class AddIndexesOnNamesMigration : Migration
+{
+    public override void Up()
+    {
+        Execute.Sql(@"
+            CREATE INDEX index_animal_name ON animals (name);
+        ");
+
+        Execute.Sql(@"
+            CREATE UNIQUE INDEX index_aviary_name ON aviaries (name);
+        ");
+    }
+
+    public override void Down()
+    {
+        Execute.Sql("DROP INDEX index_animal_name");
+
+        Execute.Sql("DROP INDEX index_aviary_name");
+    }
+}
