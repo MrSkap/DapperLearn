1c64502c64152892b41914780c9b1c8b7a7cd58b
kozlov.stepan <2001skap@gmail.com>
2025-06-06 17:20:51 +0300

    create and restore postgres dump scripts

---
+++ docker/create-dump.sh
@@ -0,0 +1,35 @@
+﻿#!/bin/bash
+
+# Parse arguments
+while getopts ":c:U:d:o:" opt; do
+  case $opt in
+    c) CONTAINER="$OPTARG" ;;
+    U) DB_USER="$OPTARG" ;;
+    d) DB_NAME="$OPTARG" ;;
+    o) DUMP_DIR="$OPTARG" ;;
+    *) echo "Invalid option"; exit 1 ;;
+  esac
+done
+
+# Set defaults
+CONTAINER=${CONTAINER:-"postgres"}
+DB_USER=${DB_USER:-"user"}
+DB_NAME=${DB_NAME:-"zoo_db"}
+DUMP_DIR=${DUMP_DIR:-"./backups"}
+
+# Create backup directory
+mkdir -p "$DUMP_DIR"
+
+# Backup filename
+DUMP_FILE="pg_dump_${DB_NAME}_$(date +%Y-%m-%d_%H-%M-%S).sql"
+
+# Execute backup in container
+echo "Creating backup from container $CONTAINER..."
+docker exec -i "$CONTAINER" pg_dump -U "$DB_USER" -d "$DB_NAME" > "${DUMP_DIR}/${DUMP_FILE}"
+
+if [ $? -eq 0 ]; then
+    echo "Backup created: ${DUMP_DIR}/${DUMP_FILE}"
+else
+    echo "Backup failed"
+    exit 1
+fi
---
+++ docker/restore-dump.sh
@@ -0,0 +1,34 @@
+﻿#!/bin/bash
+
+# Parse arguments
+while getopts ":c:U:d:f:" opt; do
+  case $opt in
+    c) CONTAINER="$OPTARG" ;;
+    U) DB_USER="$OPTARG" ;;
+    d) DB_NAME="$OPTARG" ;;
+    f) DUMP_FILE="$OPTARG" ;;
+    *) echo "Invalid option"; exit 1 ;;
+  esac
+done
+
+# Set defaults
+CONTAINER=${CONTAINER:-"postgres"}
+DB_USER=${DB_USER:-"user"}
+DB_NAME=${DB_NAME:-"zoo_db"}
+
+# Check required parameters
+if [ -z "$DUMP_FILE" ]; then
+    echo "Dump file not specified"
+    exit 1
+fi
+
+# Execute restoration in container
+echo "Restoring to container $CONTAINER..."
+docker exec -i "$CONTAINER" psql -U "$DB_USER" -d "$DB_NAME" < "$DUMP_FILE"
+
+if [ $? -eq 0 ]; then
+    echo "Database restored successfully"
+else
+    echo "Restore failed"
+    exit 1
+fi
